2eb235f6885cbffab24e2e3127ee57690b7ec1d2
c4hyo <mdcnugroho@gmail.com>
2024-09-26 20:30:04 +0700

    add variable for email and password

--- lib/pages/sign_in/controller/sign_in_controller.dart
+++ lib/pages/sign_in/controller/sign_in_controller.dart
@@ -1,3 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SignInController extends GetxController {}
+class SignInController extends GetxController {
+  // variable
+
+  // global key for form
+  GlobalKey<FormState> formSignIn = GlobalKey<FormState>();
+  // variable texteditingcontroller
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  // button function
+
+  // sign in
+
+  // sign in by google
+
+  // sign in by facebook
+
+  // sign in by apple
+
+  // sign up
+
+  // forgot password
+}
