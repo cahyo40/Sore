a39b31a241dfd24f9c4c032643bc1e90d2341382
c4hyo <mdcnugroho@gmail.com>
2024-09-26 21:41:16 +0700

    add function and variable

--- lib/pages/forgot_password/controller/forgot_password_controller.dart
+++ lib/pages/forgot_password/controller/forgot_password_controller.dart
@@ -1,3 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ForgotPasswordController extends GetxController {}
+class ForgotPasswordController extends GetxController {
+  // Global key for form
+  GlobalKey<FormState> formSignIn = GlobalKey<FormState>();
+  // variable texteditingcontroller
+  TextEditingController email = TextEditingController();
+
+  // variable for active button
+  RxBool activeBtn = false.obs;
+  // function for change activeBtn value
+  checkActiveBtn() {
+    activeBtn.value = formSignIn.currentState!.validate();
+  }
+
+  // variable loading
+  RxBool loading = false.obs;
+
+  // forgot password
+  void forgotPassword() {
+    Get.back(result: true);
+  }
+
+  // sign in
+  void signIn() {
+    Get.back();
+  }
+}
