36ccb080c3a9de1085fb21c693f07ed6651c81f5
c4hyo <mdcnugroho@gmail.com>
2024-09-26 21:15:21 +0700

    add function and variable

--- lib/pages/sign_up/controller/sign_up_controller.dart
+++ lib/pages/sign_up/controller/sign_up_controller.dart
@@ -1,3 +1,59 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SignUpController extends GetxController {}
+class SignUpController extends GetxController {
+  // Global key for form
+  GlobalKey<FormState> formSignUp = GlobalKey<FormState>();
+
+  // TextEditingController form
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmPassword = TextEditingController();
+  TextEditingController name = TextEditingController();
+
+  // variable for active btn
+  RxBool activeBtn = false.obs;
+  // function for check status of ActiveBtn
+  checkActiveBtn() {
+    activeBtn.value = formSignUp.currentState!.validate();
+  }
+
+  // variable for loading
+  RxBool loading = false.obs;
+
+  RxBool obsecureText = true.obs;
+  RxBool obsecureTextC = true.obs;
+
+  // check obsecure text active or not
+  bool isObsecureTextActive() => obsecureText.value == true;
+  bool isObsecureTextCActive() => obsecureTextC.value == true;
+
+  // change obsecure text value
+  void changeObsecureText() {
+    if (obsecureText.value == true) {
+      obsecureText.value = false;
+    } else {
+      obsecureText.value = true;
+    }
+  }
+
+  // change obsecure text confirm value
+  void changeObsecureTextC() {
+    if (obsecureTextC.value == true) {
+      obsecureTextC.value = false;
+    } else {
+      obsecureTextC.value = true;
+    }
+  }
+
+  void signUp() {
+    loading.value = true;
+    Future.delayed(const Duration(seconds: 2), () {
+      loading.value = false;
+    });
+  }
+
+  void signIn() {
+    Get.back();
+  }
+}
