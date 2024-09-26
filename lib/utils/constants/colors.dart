6ddd6631bc1ce43ea0070f74a0aa9467d0ec93f9
c4hyo <mdcnugroho@gmail.com>
2024-09-26 12:28:12 +0700

    add colors constant

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+import 'package:yo_package/yo_package.dart';
+
+Color primaryColor = YoColors.darkGreen.primary;
+Color secondaryColor = YoColors.darkGreen.secondary;
+Color textColor = YoColors.darkGreen.textColor;
+Color accentColor = YoColors.darkGreen.accentColor;
+Color backgroundColor = YoColors.darkGreen.background;
