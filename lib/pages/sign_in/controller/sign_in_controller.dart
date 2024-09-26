307c876731f88b4a3d96ce1fc9f589806c960f57
c4hyo <mdcnugroho@gmail.com>
2024-09-26 20:40:30 +0700

    add function for obsecure text and function any button in view

--- lib/pages/sign_in/controller/sign_in_controller.dart
+++ lib/pages/sign_in/controller/sign_in_controller.dart
@@ -2,25 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignInController extends GetxController {
-  // variable
-
+  // Variable
   // global key for form
   GlobalKey<FormState> formSignIn = GlobalKey<FormState>();
   // variable texteditingcontroller
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
 
-  // button function
+  // variable for password form
+  RxBool obsecureText = true.obs;
+  // check obsecure text active or not
+  bool isObsecureTextActive() => obsecureText.value == true;
+  // change obsecure text value
+  void changeObsecureText() {
+    if (obsecureText.value == true) {
+      obsecureText.value = false;
+    } else {
+      obsecureText.value = true;
+    }
+  }
 
+  // variable for active button
+  RxBool activeBtn = false.obs;
+
+  // Button function
   // sign in
-
+  void signIn() {}
   // sign in by google
-
+  void signInGoogle() {}
   // sign in by facebook
-
+  void signInFacebook() {}
   // sign in by apple
-
+  void signInApple() {}
   // sign up
-
+  void signUp() {}
   // forgot password
+  void forgotPassword() {}
 }
