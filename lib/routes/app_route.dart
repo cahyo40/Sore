d93c6d3262b087c2788809454a12e29545b22ea9
c4hyo <mdcnugroho@gmail.com>
2024-09-26 20:58:00 +0700

    fix import path

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,12 +1,13 @@
 import 'package:get/get.dart';
-import 'package:sore_book/pages/home/controller/home_binding.dart';
-import 'package:sore_book/pages/home/view/home_view.dart';
-import 'package:sore_book/pages/sign_in/controller/sign_in_binding.dart';
-import 'package:sore_book/pages/sign_in/view/sign_in_view.dart';
-import 'package:sore_book/pages/sign_up/controller/sign_up_binding.dart';
-import 'package:sore_book/pages/sign_up/view/sign_up_view.dart';
-import 'package:sore_book/pages/splashscreen/controller/splashscreen_binding.dart';
-import 'package:sore_book/pages/splashscreen/view/splashscreen_view.dart';
+
+import '../pages/home/controller/home_binding.dart';
+import '../pages/home/view/home_view.dart';
+import '../pages/sign_in/controller/sign_in_binding.dart';
+import '../pages/sign_in/view/sign_in_view.dart';
+import '../pages/sign_up/controller/sign_up_binding.dart';
+import '../pages/sign_up/view/sign_up_view.dart';
+import '../pages/splashscreen/controller/splashscreen_binding.dart';
+import '../pages/splashscreen/view/splashscreen_view.dart';
 
 part 'app_name.dart';
 
