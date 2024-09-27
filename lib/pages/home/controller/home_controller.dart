129c1d7402a59aa9a593518d1ee9f01e72a45fb6
c4hyo <mdcnugroho@gmail.com>
2024-09-27 10:03:05 +0700

    add sportlist

--- lib/pages/home/controller/home_controller.dart
+++ lib/pages/home/controller/home_controller.dart
@@ -1,3 +1,31 @@
 import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  List<Map<String, dynamic>> listSport = [
+    {
+      "title": "football",
+      "icon": MingCute.football_line,
+    },
+    {
+      "title": "badminton",
+      "icon": MingCute.badminton_line,
+    },
+    {
+      "title": "basketball",
+      "icon": MingCute.basketball_line,
+    },
+    {
+      "title": "futsal",
+      "icon": MingCute.football_line,
+    },
+    {
+      "title": "volleyball",
+      "icon": MingCute.volleyball_line,
+    },
+    {
+      "title": "other",
+      "icon": MingCute.baseball_line,
+    },
+  ];
+}
