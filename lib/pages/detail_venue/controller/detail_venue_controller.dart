abb0c4b2f483603fc80b16c7125a1f7364fa1a85
c4hyo <mdcnugroho@gmail.com>
2024-09-27 11:08:01 +0700

    add function for btn

--- lib/pages/detail_venue/controller/detail_venue_controller.dart
+++ lib/pages/detail_venue/controller/detail_venue_controller.dart
@@ -1,3 +1,17 @@
 import 'package:get/get.dart';
 
-class DetailVenueController extends GetxController {}
+class DetailVenueController extends GetxController {
+  //Button function
+
+  // button back
+  void btnBack() {
+    Get.back();
+  }
+
+  // button bookmark
+  void btnBookmark() {}
+  // button maps
+  void btnMaps() {}
+  // button booking
+  void btnBooking() {}
+}
