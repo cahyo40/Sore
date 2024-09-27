4326c43d656f40fa2a4b4339955c4232dba968c4
c4hyo <mdcnugroho@gmail.com>
2024-09-27 11:07:35 +0700

    create detail venue view

--- lib/pages/detail_venue/view/detail_venue_view.dart
+++ lib/pages/detail_venue/view/detail_venue_view.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:sore_book/pages/detail_venue/controller/detail_venue_controller.dart';
+import 'package:sore_book/utils/constants/images.dart';
+import 'package:sore_book/utils/constants/lorem.dart';
+import 'package:yo_package/yo_package.dart';
+
+import '../../../utils/constants/colors.dart';
 
 class DetailVenueView extends GetView<DetailVenueController> {
   const DetailVenueView({super.key});
@@ -8,11 +14,216 @@ class DetailVenueView extends GetView<DetailVenueController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DetailVenueView'),
+      bottomNavigationBar: BottomAppBar(
+        height: 80,
+        color: Colors.transparent,
+        child: Container(
+          height: 80,
+          width: Get.width * 0.9,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              OutlinedButton.icon(
+                onPressed: () {
+                  controller.btnMaps();
+                },
+                label: Text(
+                  "Maps",
+                  style: Get.textTheme.bodySmall,
+                ),
+                icon: Icon(
+                  Clarity.map_line,
+                  color: textColor,
+                ),
+              ),
+              YoSpace.width(),
+              Expanded(
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    controller.btnBooking();
+                  },
+                  icon: Icon(
+                    Clarity.check_line,
+                    color: textColor,
+                  ),
+                  label: Text(
+                    "Booking",
+                    style: Get.textTheme.bodySmall,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: const SafeArea(
-        child: Text('DetailVenueViewController'),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(
+              height: Get.height * 0.4,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(ImageAssets.venue),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    top: 16,
+                    left: 16,
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: InkWell(
+                        onTap: () {
+                          controller.btnBack();
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: textColor,
+                          ),
+                          child: const Icon(Iconsax.arrow_left_2_outline),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    top: 16,
+                    right: 16,
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: InkWell(
+                        onTap: () {
+                          controller.btnBookmark();
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: textColor,
+                          ),
+                          child: Icon(
+                            Clarity.bookmark_solid,
+                            color: primaryColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    bottom: 8,
+                    left: 16,
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: textColor.withOpacity(.75),
+                        ),
+                        child: Text(
+                          "Basketball",
+                          style: Get.textTheme.labelSmall!.copyWith(
+                            color: backgroundColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListView(
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              padding: YoPadding.listview,
+              children: [
+                Text(
+                  "Dawuhan Sport Center",
+                  style: Get.textTheme.titleLarge,
+                ),
+                YoSpace.height(size: 16),
+                Text(
+                  "Rp. 300.000/hour",
+                  style: Get.textTheme.labelLarge!.copyWith(
+                    color: textColor.withOpacity(.5),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Clarity.map_marker_line,
+                      color: secondaryColor,
+                      size: 16,
+                    ),
+                    YoSpace.width(),
+                    Flexible(
+                      child: Text(
+                        "Dawuhan, Kayugiyang Garung",
+                        style: Get.textTheme.bodyMedium,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Clarity.star_solid,
+                      color: secondaryColor,
+                      size: 16,
+                    ),
+                    YoSpace.width(),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "4.6 ",
+                            style: Get.textTheme.bodyMedium,
+                          ),
+                          TextSpan(
+                            text: "150 reviews",
+                            style: Get.textTheme.bodySmall!.copyWith(
+                              color: textColor.withOpacity(.5),
+                              decoration: TextDecoration.underline,
+                              decorationColor: textColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                YoSpace.height(size: 16),
+                Text(
+                  "Description",
+                  style: Get.textTheme.titleMedium,
+                ),
+                YoSpace.height(),
+                Text(
+                  lorem,
+                  textAlign: TextAlign.justify,
+                  style: Get.textTheme.bodyMedium!.copyWith(
+                    color: textColor.withOpacity(.75),
+                  ),
+                ),
+                Text(
+                  lorem,
+                  textAlign: TextAlign.justify,
+                  style: Get.textTheme.bodyMedium!.copyWith(
+                    color: textColor.withOpacity(.75),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
