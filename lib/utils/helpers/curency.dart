cb08012af78079ec795f568b2329ead3feb3bbab
c4hyo <mdcnugroho@gmail.com>
2024-10-02 07:50:11 +0700

    create currency helper

---
+++ lib/utils/helpers/curency.dart
@@ -0,0 +1,12 @@
+import 'package:intl/intl.dart';
+
+class CurrencyHelper {
+  static String rupiah(int amount) {
+    final formatter = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: 'Rp ',
+      decimalDigits: 0,
+    );
+    return formatter.format(amount);
+  }
+}
